3294f8a08ec69bea41f3c51ab38b06c4c6643bfe
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-05-14 03:26:10 +0530

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,10 @@ import "./globals.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
-
-export default function RootLayout({
-  children,
-  pageProps,
-}: {
-  children: React.ReactNode;
-  pageProps: any;
-}) {
+export default function RootLayout({ children, pageProps }: any) {
   const client = createBrowserSupabaseClient();
   return (
-    <html lang="en" >
+    <html lang="en">
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           <SessionContextProvider
