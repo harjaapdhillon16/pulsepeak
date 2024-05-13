cc788f0104e7b0bcd0de2c196812af63c1f3eced
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-05-14 03:24:35 +0530

    change

---
+++ app/api/app/create_diet/route.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '../../utils/supabase';
+import { NextResponse } from 'next/server';
+
+export const POST = async (req:any, res:NextApiResponse) => {
+  const { body, table } = await req.json();
+  const { error, data } = await supabase.from(table).insert(body).select();
+  return NextResponse.json({ error, data });
+};
