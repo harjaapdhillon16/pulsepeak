2219c4e66c646e2878877ebcefe6ea7aac256383
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-06-30 17:03:29 +0530

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,9 +7,9 @@ export const Footer = () => {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center mr-4 dark:text-gray-400">
           © 2024{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <Link href="/" className="hover:underline">
             Pulsepeak
-          </a>
+          </Link>
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
