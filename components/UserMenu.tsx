b48db31f592776600eee48efdd38434d8b184e65
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-07-04 06:55:55 +0530

    changes

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -59,14 +59,16 @@ export const UserMenu = ({ isSubscribed }: { isSubscribed: boolean }) => {
             </p>
           </CardBody>
           <CardFooter className="gap-3">
-            <a
-              target="_blank"
-              href={linkToSub}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              rel="noopener"
-            >
-              Subscribe for just INR 29/ Month
-            </a>
+            {Boolean(linkToSub) && (
+              <a
+                target="_blank"
+                href={linkToSub}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                rel="noopener"
+              >
+                Subscribe for just INR 29/ Month
+              </a>
+            )}
           </CardFooter>
         </Card>
       )}
