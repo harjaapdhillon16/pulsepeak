6d9406bf0ec2ce56a29725d314706fc031af0690
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-07-04 06:54:19 +0530

    changes

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "@/utils/hooks/useSupabase";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import {
   Card,
@@ -7,12 +7,27 @@ import {
   CardBody,
   CardFooter,
   Avatar,
-  Button,
 } from "@nextui-org/react";
 import axios from "axios";
 
 export const UserMenu = ({ isSubscribed }: { isSubscribed: boolean }) => {
   const { supabaseUser } = useAuth();
+  const [linkToSub, setLinkToSub] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      if (supabaseUser?.id && !isSubscribed) {
+        const { data } = await axios.post(
+          "/api/razorpay/create-subscription-link",
+          {
+            user_id: supabaseUser?.id,
+            email: supabaseUser?.email,
+          }
+        );
+        setLinkToSub(data);
+      }
+    })();
+  }, [supabaseUser, isSubscribed]);
   return (
     <div className="w-full px-4 min-h-[70vh]">
       <p className="text-2xl">Hey {supabaseUser?.full_name}</p>
@@ -44,21 +59,14 @@ export const UserMenu = ({ isSubscribed }: { isSubscribed: boolean }) => {
             </p>
           </CardBody>
           <CardFooter className="gap-3">
-            <Button
-              onClick={async () => {
-                const { data } = await axios.post(
-                  "/api/razorpay/create-subscription-link",
-                  {
-                    user_id: supabaseUser?.id,
-                    email: supabaseUser?.email,
-                  }
-                );
-                window.open(data);
-              }}
-              color="primary"
+            <a
+              target="_blank"
+              href={linkToSub}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              rel="noopener"
             >
               Subscribe for just INR 29/ Month
-            </Button>
+            </a>
           </CardFooter>
         </Card>
       )}
