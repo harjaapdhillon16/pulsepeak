9106259a8ef941632523863161c9667e528f19b4
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-07-04 20:38:27 +0530

    Changes

--- app/api/razorpay/create-subscription-link/route.ts
+++ app/api/razorpay/create-subscription-link/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export const POST = async (req: any) => {
   const { user_id, email } = await req.json();
   let dataToSend = JSON.stringify({
-    plan_id: "plan_OULVh022gQ6rK4",
+    plan_id: "plan_OUbCVEGaA370XM",
     total_count: 100,
     quantity: 1,
     customer_notify: 1,
